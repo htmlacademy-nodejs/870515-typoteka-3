a5644826c9524797f5bbec3dc86a3c56fcb8b5af
Nemhis <p.nemphis@gmail.com>
2021-08-10 00:48:41 +0300

    Refactor generatePosts input args

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,7 +2,6 @@ const fs = require(`fs`);
 const chalk = require('chalk');
 
 const {getRandomInt, shuffle, readMocks, getRandomDate} = require(`../../utils`);
-const {ExitCode} = require(`../../constants`);
 
 const FILE_NAME = `mocks.json`;
 const TITLE_MOCKS_PATH = `src/data/titles.txt`;
@@ -13,7 +12,7 @@ const MAX_COUNT = 1000;
 const DEFAULT_COUNT = 1;
 const DATE_RANGE = 90 * 24 * 60 * 60 * 1000; // ~ 3 месяца
 
-const generatePost = (titles, sentences, categories) => ({
+const generatePost = ({ titles, sentences, categories }) => ({
   title: titles[getRandomInt(0, titles.length)],
   createdDate: getRandomDate(DATE_RANGE),
   announce: shuffle(sentences).slice(0, getRandomInt(1, 5)),
@@ -21,7 +20,7 @@ const generatePost = (titles, sentences, categories) => ({
   category: shuffle(categories).slice(0, getRandomInt(1, categories.length)),
 });
 
-const generatePosts = (count, titles, sentences, categories) => Array(count).fill({}).map(() => generatePost(titles, sentences, categories));
+const generatePosts = (count, mocks) => Array(count).fill({}).map(() => generatePost(mocks));
 
 module.exports = {
   name: `--generate`,
@@ -36,7 +35,7 @@ module.exports = {
       throw new Error(`Не больше 1000 публикаций`);
     }
 
-    let content = JSON.stringify(generatePosts(postsCount, titles, sentences, categories));
+    const content = JSON.stringify(generatePosts(postsCount, { titles, sentences, categories }));
 
     try {
       await fs.promises.writeFile(FILE_NAME, content);
