d4b642ff35bd674b502217e65903cc46623a01ae
Nemhis <p.nemphis@gmail.com>
2021-09-09 01:49:48 +0300

    Add route group name to response

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -5,6 +5,6 @@ const routes = new Router();
 routes.get(`/add`, (req, res) => res.send(`/articles/add`));
 routes.get(`/:id`, (req, res) => res.send(`/articles/${req.params.id}`));
 routes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/${req.params.id}`));
-routes.get(`/category/:id`, (req, res) => res.send(`/category/${req.params.id}`));
+routes.get(`/category/:id`, (req, res) => res.send(`/articles/category/${req.params.id}`));
 
 module.exports = routes;
