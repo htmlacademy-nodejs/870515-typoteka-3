98e90e0ffde2bbff12e93aad50cda48190fc6d17
Nemhis <p.nemphis@gmail.com>
2021-09-09 01:47:36 +0300

    Remove regexp in router

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get(`/:id([0-9]+)`, (req, res) => res.send(`/articles/:id([0-9]+)`));
-routes.get(`/edit/:id([0-9]+)`, (req, res) => res.send(`/articles/edit/:id([0-9]+)`));
 routes.get(`/add`, (req, res) => res.send(`/articles/add`));
-routes.get(`/category/:id([0-9]+)`, (req, res) => res.send(`/category/:id([0-9]+)`));
+routes.get(`/:id`, (req, res) => res.send(`/articles/${req.params.id}`));
+routes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/${req.params.id}`));
+routes.get(`/category/:id`, (req, res) => res.send(`/category/${req.params.id}`));
 
 module.exports = routes;
