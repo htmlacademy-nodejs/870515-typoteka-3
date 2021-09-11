34caaba6059c272620ebe7212b98cde369922ce1
Nemphis <p.nemphis@gmail.com>
2021-09-11 21:09:38 +0300

    Init pug

--- src/express/express.js
+++ src/express/express.js
@@ -1,14 +1,23 @@
-const express = require(`express`);
-const myRoutes = require(`./routes/my-routes`)
-const articlesRoutes = require(`./routes/articles-routes`)
+'use strict';
 
+const express = require(`express`);
+const path = require(`path`);
+const myRoutes = require(`./routes/my-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
+
+const PUBLIC_DIR = `public`;
 const DEFAULT_PORT = 8080;
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+// routing
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
-app.get(`/`, (req, res) => res.send(`/`));
+app.get(`/`, (req, res) => res.render(`main`));
 app.get(`/register`, (req, res) => res.send(`/register`));
 app.get(`/login`, (req, res) => res.send(`/login`));
 app.get(`/categories`, (req, res) => res.send(`/categories`));
