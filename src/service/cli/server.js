eda1a99180ccebce3efe9076fd0acd57d80d838d
Nemhis <p.nemphis@gmail.com>
2021-09-18 20:03:08 +0300

    Create api express server

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,32 +1,14 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`);
 const {HttpStatus} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
-const HOST = `localhost`;
-const SCHEME = `http`;
 
 const getMocks = async () => {
   const posts = await fs.promises.readFile(`./mocks.json`, `utf-8`);
   return JSON.parse(posts);
-};
-
-const renderPosts = (posts) => {
-  return `<ul>
-    ${posts.map((post) => `<li>${post.title}</li>`).join(``)}
- </ul>`;
-};
-
-const renderPage = (content) => {
-  return `<html lang="ru">
-    <head>
-      <title>My page</title>
-    </head>
-    <body>${content}</body>
-  </html>
-  `;
 };
 
 module.exports = {
@@ -34,29 +16,30 @@ module.exports = {
   async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+    const app = express();
 
-    const server = http.createServer(async (request, response) => {
+    app.use(express.json());
+
+    app.get(`/posts`, async (request, response) => {
       let posts;
 
       try {
         posts = await getMocks();
-        const content = renderPosts(posts);
 
-        response.writeHead(HttpStatus.success, {
-          'Content-Type': `text/html; charset=UTF-8`,
-        });
-
-        response.end(renderPage(content));
+        response.send(posts);
       } catch (error) {
-        response.writeHead(HttpStatus.notFound, {
-          'Content-Type': `text/html; charset=UTF-8`,
-        });
-        response.end(`Not found`);
+        response
+          .status(HttpStatus.notFound)
+          .send(`Not found`);
       }
     });
 
-    server.listen(port, () => {
-      console.info(`Принимаю подключения на ${SCHEME}://${HOST}:${port}`);
+    app.use((req, res) => res
+      .status(HttpStatus.notFound)
+      .send(`Not found`));
+
+    app.listen(port, () => {
+      console.log(`Server was started http://localhost:${port}`);
     });
   }
 };
