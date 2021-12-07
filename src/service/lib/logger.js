fb98f2a5c0e29bfab809662179ba7e99035966f0
Nemhis <p.nemphis@gmail.com>
2021-12-07 22:54:53 +0300

    Log level from env

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const pino = require(`pino`);
+const {Env} = require(`../../constants`);
+
+console.log(process.env.LOG_LEVEL);
+const LOG_FILE = `./logs/api.log`;
+const isDevMode = process.env.NODE_ENV === Env.development;
+const defaultLogLevel = isDevMode ? `info` : `error`;
 
 const logger = pino({
   name: `base-logger`,
-  level: `info`,
-  transport: {
-    target: `pino-pretty`,
-  }
-});
+  level: process.env.LOG_LEVEL || defaultLogLevel,
+  transport: {target: `pino-pretty`},
+}, isDevMode ? process.stdout : pino.destination(LOG_FILE));
 
 module.exports = {
   logger,
